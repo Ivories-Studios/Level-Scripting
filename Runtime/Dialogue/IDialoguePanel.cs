32e72767353de04030026c8cd861a63a3111a819
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-09-18 23:56:57 +0200

    Update IDialoguePanel.cs

--- Runtime/Dialogue/IDialoguePanel.cs
+++ Runtime/Dialogue/IDialoguePanel.cs
@@ -2,7 +2,7 @@ using IvoriesStudios.LevelScripting.Dialogue;
 
 public interface IDialoguePanel
 {
-    public static IDialoguePanel Instance { get; }
+    public static IDialoguePanel Instance { get; protected set; }
     public void StartDialogue();
     public void SetDialogueLine(Character speaker, string line);
     public void StopDialogue();
